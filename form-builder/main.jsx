c3fc0f8b0bdd41bf40cc4a612d34501f56be5214
Om Gupta <omgstar0014@gmail.com>
2025-06-01 13:33:50 +0530

    Update main.jsx

--- form-builder/main.jsx
+++ form-builder/main.jsx
@@ -1,5 +1,6 @@
-import React from "https://esm.sh/react@18";
-import ReactDOM from "https://esm.sh/react-dom@18";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
