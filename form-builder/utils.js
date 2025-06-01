b7ecad29033745b684dca6a3b6f1a030961ae557
Om Gupta <omgstar0014@gmail.com>
2025-06-01 13:31:07 +0530

    Update utils.js

--- form-builder/utils.js
+++ form-builder/utils.js
@@ -1,19 +1,4 @@
 export function generateId() {
-  return Math.random().toString(36).substr(2, 9);
-}
-
-export function saveFormToLocalStorage(id, form) {
-  localStorage.setItem(`form-${id}`, JSON.stringify(form));
-}
-
-export function loadFormFromLocalStorage(id) {
-  const data = localStorage.getItem(`form-${id}`);
-  return data ? JSON.parse(data) : null;
-}
-
-export function copyToClipboard(text) {
-  navigator.clipboard.writeText(text).then(
-    () => alert("Link copied to clipboard!"),
-    () => alert("Failed to copy link.")
-  );
+// Simple unique id generator
+return Math.random().toString(36).substr(2, 9);
 }
